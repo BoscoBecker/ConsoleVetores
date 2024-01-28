87589aa1d013acfbf2e3ee79cc470217a1f05b93
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-28 19:51:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleVetores.Vetor;
+
+namespace ConsoleVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VetorArray Vetarray = new VetorArray();
+            Console.Write("Informe a quantidade de Valores:");
+            int valor = int.Parse(Console.ReadLine());
+            Vetarray.AdicionarQuantidadeVetor(valor);
+        }
+    }
+}
---
+++ Vetor/Vetor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ConsoleVetores.Vetor
+{
+    class VetorArray
+    {
+        private double[] vet;
+        private double sum;
+
+        public void AdicionarQuantidadeVetor(int quantidade) {
+            vet = new double[quantidade];
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                sum += vet[i];
+            }
+            Console.WriteLine("A média é: " + this.soma(sum,quantidade));
+
+        }
+        private double soma(double soma, double quantidade){
+            return (soma / quantidade);
+            
+         }
+        
+    }
+}
